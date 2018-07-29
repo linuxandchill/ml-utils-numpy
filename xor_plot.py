efabba24a93ce12678df624b1b77077df041a863
linuxandchill <rivercodi@gmail.com>
2018-07-29 16:19:52 -0700

    make linearly separable

--- xor_plot.py
+++ xor_plot.py
@@ -15,5 +15,11 @@ targs = np.array([0,1,1,0])
 
 ones = np.array([[1]*num]).T
 
+#turn it into 3D problem in order to solve it
+#can draw a plane between datasets w 3D
+
+xy = np.matrix(X[:,0] * X[:, 1]).T
+Xb = np.array(np.concatenate((ones, xy, X), axis=1))
+
 plt.scatter( X[:, 0], X[:,1], c=targs)
 plt.show()
